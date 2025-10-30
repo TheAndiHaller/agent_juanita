078455e75f31723b7657bf9c5a12dbf07f48226c
Andi <andi_haller@hotmail.com>
2025-10-30 19:12:37 -0300

    ADD get_files_info function

---
+++ tests.py
@@ -0,0 +1,9 @@
+from functions.get_files_info import get_files_info
+
+try:
+    print(get_files_info("calculator", "."))
+    print(get_files_info("calculator", "pkg"))
+    print(get_files_info("calculator", "/bin"))
+    print(get_files_info("calculator", "../"))
+except Exception as e:
+    print(f"Error: {e}")
