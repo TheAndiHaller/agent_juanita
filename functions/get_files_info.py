4878e6ad3a678806c1c7035d67a06a0d005b9834
Andi <andi_haller@hotmail.com>
2025-10-30 20:53:17 -0300

    ADD get_file_content function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,27 +5,29 @@ def get_files_info(working_directory, directory="."):
     working_directory_abs_path = os.path.abspath(working_directory)
     directory_path = os.path.abspath(os.path.join(working_directory_abs_path, directory))
 
+    if not directory_path.startswith(working_directory_abs_path):
+        return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
+
     if not os.path.exists(directory_path):
         return f"Error: \"{directory}\" does not exist"
 
     if not os.path.isdir(directory_path):
         return f"Error: \"{directory}\" is not a directory"
 
-    if not directory_path.startswith(working_directory_abs_path):
-        #raise Exception(f"Cannot list \"{directory}\" as it is outside the permitted working directory")
-        return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
+
 
     result = []
-    result.append("Result for current directory:")
     dir_content = os.listdir(directory_path)
 
-    for entry in dir_content:
-        entry_path = os.path.abspath(os.path.join(directory_path, entry))
-        file_size = os.path.getsize(entry_path)
-        is_dir = os.path.isdir(entry_path)
+    try:
+        for entry in dir_content:
+            entry_path = os.path.abspath(os.path.join(directory_path, entry))
+            file_size = os.path.getsize(entry_path)
+            is_dir = os.path.isdir(entry_path)
 
-        result.append(f" - {entry}: file_size={file_size} bytes, is_dir={is_dir}")
-    
-    return "\n".join(result)
+            result.append(f" - {entry}: file_size={file_size} bytes, is_dir={is_dir}")
 
+        return "\n".join(result)
+    except Exception as e:
+        return f"Error listing files: {e}"
 
--- tests.py
+++ tests.py
@@ -1,9 +1,23 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-try:
-    print(get_files_info("calculator", "."))
-    print(get_files_info("calculator", "pkg"))
-    print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../"))
-except Exception as e:
-    print(f"Error: {e}")
+#print("Result for current directory:")
+#print(get_files_info("calculator", "."))
+#print("")
+#print("Result for current directory:")
+#print(get_files_info("calculator", "pkg"))
+#print("")
+#print("Result for current directory:")
+#print(get_files_info("calculator", "/bin"))
+#print("")
+#print("Result for current directory:")
+#print(get_files_info("calculator", "../"))
+
+
+#print("File lorem.txt")
+#print(get_file_content("calculator", "lorem.txt"))
+
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat"))
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
