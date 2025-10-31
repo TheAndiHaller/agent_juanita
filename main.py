8d53f58a66f21dba58c3f7ab7acf08cb0ce2aa26
Andi <andi_haller@hotmail.com>
2025-10-31 13:45:59 -0300

    ADD function call to model

--- main.py
+++ main.py
@@ -3,41 +3,68 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-# Verbose flag
-verbose = False
+    client = genai.Client(api_key=api_key)
 
-if len(sys.argv) < 2:
-    print("Error: ADD a prompt as argument")
-    sys.exit(1)
+    # Verbose flag
+    verbose = False
 
-if "--verbose" in sys.argv:
-    verbose = True
+    if len(sys.argv) < 2:
+        print("Error: ADD a prompt as argument")
+        sys.exit(1)
 
-user_prompt = sys.argv[1]
+    if "--verbose" in sys.argv:
+        verbose = True
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
+    user_prompt = sys.argv[1]
 
-system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    # History of the conversation
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001", 
-    contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
-)
+    # Funtion list / Tools
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
 
-if verbose:
-    print(f"User prompt: {user_prompt}")
-    print("")
-print(response.text)
-if verbose:    
-    print("")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # System prompt
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+    )
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print("")
+
+    print(response.text)
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+    if verbose:    
+        print("")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+if __name__ == "__main__":
+    main()
